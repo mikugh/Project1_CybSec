70fa5f86fb6a11f8abf547802ed61e5197a205f2
mikugh <mikael.granqvist@abo.fi>
2024-05-18 13:21:05 +0300

    Final

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -2,8 +2,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
-
+# Class for creating the results view
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy("login")
+    success_url = reverse_lazy("login") #Code to redirect the user to the login page after successful sign up
     template_name = "registration/signup.html"
