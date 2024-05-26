1e2ed4baea878c258745442dd072620805f63c6e
mikugh <mikael.granqvist@abo.fi>
2024-05-26 12:55:47 +0300

    updated

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -48,8 +48,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #Uncomment line under this code to fix flaw #1!
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
